f66fb88c6906ad3bae033a3991113940e934c5ba
John Zaitseff <J.Zaitseff@zap.org.au>
2011-06-14 18:19:01 +1000

    Create preliminary versions of the source code and manual page

---
+++ src/trader.h
@@ -0,0 +1,42 @@
+/************************************************************************
+*                                                                       *
+*             Star Traders: A Game of Interstellar Trading              *
+*                Copyright (C) 1990-2011, John Zaitseff                 *
+*                                                                       *
+************************************************************************/
+
+/*
+  Author:  John Zaitseff <J.Zaitseff@zap.org.au>
+  Date:    14th June, 2011
+  Version: 7.0
+
+  $Id$
+
+  Star Traders is a simple game of interstellar trading, where the object
+  of the game is to create companies, buy and sell shares, borrow and
+  repay money, in order to become the wealthiest player (the winner).
+
+  This file, "trader.h", contains default game values and certain other
+  definitions for Star Traders.
+
+
+  This program is free software: you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by the
+  Free Software Foundation, either version 3 of the License, or (at your
+  option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see http://www.gnu.org/licenses/.
+*/
+
+
+#ifndef __TRADER_H__
+#define __TRADER_H__
+
+
+#endif /* __TRADER_H__ */
