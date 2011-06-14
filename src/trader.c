305be1e85248212998921d3d0df4bc4792b7a6d3
John Zaitseff <J.Zaitseff@zap.org.au>
2011-06-14 23:02:16 +1000

    Add a simple main() routine for testing

--- src/trader.c
+++ src/trader.c
@@ -29,3 +29,13 @@
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see http://www.gnu.org/licenses/.
 */
+
+
+/************************************************************************
+*                             Main program                              *
+************************************************************************/
+
+int main (int argc, char *argv[])
+{
+    return EXIT_SUCCESS;
+}
