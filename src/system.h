48e8bbba93287c219e885c0841e0e43de72407cc
John Zaitseff <J.Zaitseff@zap.org.au>
2011-07-28 21:19:33 +1000

    Complete rewrite of the M4 macro that checks for Curses

--- src/system.h
+++ src/system.h
@@ -76,14 +76,18 @@
 
 // X/Open-compatible Curses library
 
-#if defined(HAVE_NCURSESW) && defined(HAVE_NCURSESW_H)
+#if defined(HAVE_NCURSESW_CURSES_H)
 #  include <ncursesw/curses.h>
+#elif defined(HAVE_NCURSESW_H)
+#  include <ncursesw.h>
+#elif defined(HAVE_NCURSES_CURSES_H)
+#  include <ncurses/curses.h>
 #elif defined(HAVE_NCURSES_H)
 #  include <ncurses.h>
 #elif defined(HAVE_CURSES_H)
 #  include <curses.h>
 #else
-#  error "X/Open-compatible Curses library required"
+#  error "X/Open-compatible Curses header file required"
 #endif
 
 
